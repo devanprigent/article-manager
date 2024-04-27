c3937995af066b409cae499722cbe2a4b5145fa4
devan <devan.prgt@gmail.com>
2024-04-27 18:12:16 +0200

    Changed Tag's input design

--- frontend/src/components/Forms/FormTags.tsx
+++ frontend/src/components/Forms/FormTags.tsx
@@ -47,7 +47,7 @@ function Tags({ onChange, currentTags }: Readonly<TagsProps>) {
       <label htmlFor="tags">
         <b>Tags</b>
       </label>
-      <div className="border border-gray-300 p-2 rounded flex items-center flex-wrap gap-2">
+      <div className="border border-gray-300 bg-white p-2 rounded flex items-center flex-wrap gap-2">
         {tags.map((tag, index) => (
           <div
             className="flex flex-row space-x-2 bg-gray-300 px-3 py-2 rounded-full items-center"
@@ -69,7 +69,7 @@ function Tags({ onChange, currentTags }: Readonly<TagsProps>) {
           name="tags"
           onKeyDown={handleKeyDown}
           type="text"
-          className="flex-grow p-y-2 border-none outline-gray-50 bg-gray-50"
+          className="flex-grow p-y-2 border-none outline-none bg-white"
         />
       </div>
     </>
