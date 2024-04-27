c5fa9f310f6fb01dcdaac89186aa5016fba45f24
devan <devan.prgt@gmail.com>
2024-04-27 09:49:20 +0200

    Prevented the upload of an article with the same title and author that an existing one

--- backend/article/views.py
+++ backend/article/views.py
@@ -11,6 +11,11 @@ class ArticleSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
+        # Check if an article with the same title and author already exists
+        article = Article.objects.filter(name=validated_data['name'], author=validated_data['author'])
+        if article.exists():
+            raise serializers.ValidationError("Article with the same name and author already exists")
+        # Get the tags data from the request
         tags_data = self.context['request'].data.get('tags', [])
         validated_tags = []
         for tag_data in tags_data:
